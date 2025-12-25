1a5969b6421b29f5cec11ecffce113b0f390dd20
etnard <drantelme@gmail.com>
2025-12-24 17:10:02 -0800

    Terraform

--- s3/iac/terraform/s3.tf
+++ s3/iac/terraform/s3.tf
@@ -1,5 +1,5 @@
-resource "aws_s3_bucket" "emy-s3-bucket" {
-  bucket = "my-tf-test-bucket"
+resource "aws_s3_bucket" "my-s3-bucket" {
+  bucket = "my-tf-test-bucket-etnard"
 
   tags = {
     Name        = "My bucket"
